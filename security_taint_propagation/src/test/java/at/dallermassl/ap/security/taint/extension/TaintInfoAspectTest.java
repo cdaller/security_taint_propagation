2783a806017661e295753d4f3c001105a7fde091
Christof Dallermassl <christof@dallermassl.at>
2013-02-19 20:14:56 +0100

    removed taint info aspect again - crashes the vm on startup :-(

--- security_taint_propagation/src/test/java/at/dallermassl/ap/security/taint/extension/TaintInfoAspectTest.java
+++ security_taint_propagation/src/test/java/at/dallermassl/ap/security/taint/extension/TaintInfoAspectTest.java
@@ -4,14 +4,15 @@ import org.aspectj.lang.Aspects;
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class TaintInfoAspectTest {
 
     @Test
     public void testTaintInfoAspect() {
-//       String foo = new String("foo");
-//       Assert.assertFalse(Aspects.hasAspect(TaintInfoAspect.class, foo));
-//       foo.setTainted(true);
-//       Assert.assertTrue(Aspects.hasAspect(TaintInfoAspect.class, foo));       
+       String foo = new String("foo");
+       Assert.assertFalse(Aspects.hasAspect(TaintInfoAspect.class, foo));
+       foo.setTainted(true);
+       Assert.assertTrue(Aspects.hasAspect(TaintInfoAspect.class, foo));       
     }
 
 }
